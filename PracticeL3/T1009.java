c5379dd731b36c554a8239f30a39a17bc1802f4e
ShiaoYoung <117451424+ShiaoYoung@users.noreply.github.com>
2023-01-16 22:11:46 +0800

    Add files via upload

--- PracticeL3/T1009.java
+++ PracticeL3/T1009.java
@@ -8,20 +8,47 @@ public class T1009 {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String result = "";
-
-        //2. 倒着输出
-        String temp = "";
+        
+        //2. 统计空格数量
+        int count = 0;
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == ' ' || i == input.length() - 1){
-                result = temp + " " + result;
-                temp = "";
-                i++;
+            if (input.charAt(i) == ' '){
+                count++;
             }
-            if (i < input.length())
-                temp += input.charAt(i);
+        }
+        count++;
+
+        //3. 创建数组，存各个单词
+        String[] words = new String[count];
+
+        //4. 单词存入数组
+        int count1 = count - 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            sb.append(input.charAt(i));
+            if (i == input.length() - 1){
+                words[count1] = sb.toString();
+                sb = new StringBuilder("");
+                count1--;
+                i++;
+                break;
+            }
+            if (input.charAt(i + 1) == ' ' ){
+                words[count1] = sb.toString();
+                sb = new StringBuilder("");
+                count1--;
+                i++;
+                continue;
+            }
         }
 
-        //3. 输出结果
-        System.out.println(result);
+        //5. 输出结果
+        for (int i = 0; i < words.length; i++) {
+            if (i == words.length - 1){
+                System.out.print(words[i]);
+            }else {
+                System.out.print(words[i] + " ");
+            }
+        }
     }
 }
